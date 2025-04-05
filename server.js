9c6ebcf489e3251768d22d095d8b757111b682f5
Jorge Morel <Jorge.morel@kinetic.auto>
2025-04-04 17:06:02 -0700

    Fix: use Render-assigned port

--- server.js
+++ server.js
@@ -37,5 +37,6 @@ app.post('/addresses', (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
